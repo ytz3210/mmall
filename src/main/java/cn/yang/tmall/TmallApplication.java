2297fd9aff23cba9c63b1ccf21c31da3d8c81ce8
yangtz <yangtzl@foxmail.com>
2020-01-29 11:20:01 +0800

    add 增加log4j2日志彩色打印

--- src/main/java/cn/yang/tmall/TmallApplication.java
+++ src/main/java/cn/yang/tmall/TmallApplication.java
@@ -1,12 +1,9 @@
 package cn.yang.tmall;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
-//@ImportResource(value = {"classpath:generatorConfig.xml"})
 public class TmallApplication {
 
     public static void main(String[] args) {
