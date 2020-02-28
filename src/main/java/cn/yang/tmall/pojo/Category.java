e4936d020f3ad925c917b45fb6d11cae2476adf8
yangtz <yangtzl@foxmail.com>
2020-02-28 23:03:58 +0800

    fixup! update 优化实体类字段

--- src/main/java/cn/yang/tmall/pojo/Category.java
+++ src/main/java/cn/yang/tmall/pojo/Category.java
@@ -1,11 +1,9 @@
 package cn.yang.tmall.pojo;
 
-import lombok.Data;
-
+import java.util.Date;
 import java.util.Objects;
 
-@Data
-public class Category extends BaseEntity{
+public class Category {
     private Integer id;
 
     private Integer parentId;
@@ -15,6 +13,80 @@ public class Category extends BaseEntity{
     private Boolean status;
 
     private Integer sortOrder;
+
+    private Date createTime;
+
+    private Date updateTime;
+
+    public Category(Integer id, Integer parentId, String name, Boolean status, Integer sortOrder, Date createTime, Date updateTime) {
+        this.id = id;
+        this.parentId = parentId;
+        this.name = name;
+        this.status = status;
+        this.sortOrder = sortOrder;
+        this.createTime = createTime;
+        this.updateTime = updateTime;
+    }
+
+    public Category() {
+        super();
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getParentId() {
+        return parentId;
+    }
+
+    public void setParentId(Integer parentId) {
+        this.parentId = parentId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public Boolean getStatus() {
+        return status;
+    }
+
+    public void setStatus(Boolean status) {
+        this.status = status;
+    }
+
+    public Integer getSortOrder() {
+        return sortOrder;
+    }
+
+    public void setSortOrder(Integer sortOrder) {
+        this.sortOrder = sortOrder;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
 
     @Override
     public boolean equals(Object o) {
